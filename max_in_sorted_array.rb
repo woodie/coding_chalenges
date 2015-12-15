02fc70cf72f85f15b27b2e9821adeebb2ee0b127
John Woodell <woodie@netpress.com>
2015-12-14 17:32:28 -0800

    clean up logic

--- max_in_sorted_array.rb
+++ max_in_sorted_array.rb
@@ -1,12 +1,10 @@
-#!/usr/bin/env ruby
-
 data = (1..9).to_a.rotate!.rotate!.rotate!
 
 def find_max(a)
   return a.first if a.size == 1
-  return a[-1] if a[0] < a[-1]
+  return a.last if a.first < a.last
   pivot = a.size / 2
-  if pivot > a[-1]
+  if a[pivot] > a.last
     find_max(a[pivot..-1])
   else
     find_max(a[0..(pivot - 1)])
