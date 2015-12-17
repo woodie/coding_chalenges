96b8263bc266b5318fe634f8f9191c326d404b0b
John Woodell <woodie@netpress.com>
2015-12-16 16:05:54 -0800

    Generate resizable board in a function

--- artboard.js
+++ artboard.js
@@ -2,18 +2,6 @@
 
 "use strict";
 
-var board = [
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,1,1,1,1,0,0,0,0],
-    [0,0,1,1,1,1,0,0,0,0],
-    [0,0,1,1,1,1,0,0,0,0],
-    [0,0,1,1,1,1,0,0,0,0],
-    [0,0,1,1,1,1,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0]];
-
 function coordinates_naive(img) {
   var x, y, w, h;
   for (var r = 0; r < img.length; r++) {
@@ -58,6 +46,25 @@ function coordinates_smart(img) {
   return null;
 }
 
+function plot_rect_on_board(x, y, w, h) {
+  var min = 10;
+  var height = (y + h) > min ? (y + h) : min
+  var width  = (x + w) > min ? (x + w) : min
+  board = new Array(height);
+  for (let row = 0; row < height; row++) {
+    board[row] = Array(width);
+    for (let col = 0; col < width; col++) {
+      board[row][col] = (
+          row >= x + 1 &&
+          col >= y - 1 &&
+          col <= w + x - 1 &&
+          row <= h + y - 1
+      ) ? 1 : 0;
+    }
+  }
+  return board;
+}
+
 function render_entire_artboard(img) {
   var buffer = '\n   ';
   for (var n = 0; n < img[0].length; n++) {
@@ -76,6 +83,25 @@ function render_entire_artboard(img) {
   console.log(buffer);
 }
 
+var board = plot_rect_on_board(2,3,4,5);
 render_entire_artboard(board);
 console.log(coordinates_naive(board));
 console.log(coordinates_smart(board));
+
+/*
+    0 1 2 3 4 5 6 7 8 9
+  0 . . . . . . . . . .
+  1 . . . . . . . . . .
+  2 . . . . . . . . . .
+  3 . . X X X X . . . .
+  4 . . X X X X . . . .
+  5 . . X X X X . . . .
+  6 . . X X X X . . . .
+  7 . . X X X X . . . .
+  8 . . . . . . . . . .
+  9 . . . . . . . . . .
+
+{ x: 2, y: 3, w: 4, h: 5 }
+{ x: 2, y: 3, w: 4, h: 5 }
+
+*/
