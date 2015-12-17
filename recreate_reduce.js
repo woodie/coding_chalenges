2d688c27c8ee5de2998c019e503a90345d3c0ce1
John Woodell <woodie@netpress.com>
2015-12-16 23:08:07 -0800

    Call our SIMPLIFIED reduce method inject

--- recreate_reduce.js
+++ recreate_reduce.js
@@ -2,7 +2,7 @@
 
 "use strict";
 
-Array.prototype.deduce = function(funct) {
+Array.prototype.inject = function(funct) {
   var m = (typeof this[0] === 'number') ? 0 : '';
   for (let i = 0; i < this.length; i++) {
     m = funct(m, this[i]);
@@ -11,9 +11,9 @@ Array.prototype.deduce = function(funct) {
 };
 
 console.log(['a','b','c','d','e','f'].reduce(function(m,c){return m + c}));
-console.log(['a','b','c','d','e','f'].deduce(function(m,c){return m + c}));
+console.log(['a','b','c','d','e','f'].inject(function(m,c){return m + c}));
 console.log([1, 2, 3, 4, 5].reduce(function(m,c){return m + c}));
-console.log([1, 2, 3, 4, 5].deduce(function(m,c){return m + c}));
+console.log([1, 2, 3, 4, 5].inject(function(m,c){return m + c}));
 
 /*
 
