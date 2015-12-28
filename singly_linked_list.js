8ebb971f9410b5ebfd2368eca0388c46d140f4c6
John Woodell <woodie@netpress.com>
2015-12-27 19:36:35 -0800

    Update ES5 version with palindromal

--- singly_linked_list.js
+++ singly_linked_list.js
@@ -15,10 +15,10 @@ var NiceList = (function () {
     this.head = head;
   }
 
-  // [a]→ [b]→ [c]→ [d]→ [e]→ [f]→ [g]→ [h]→ [i]→ [j]
+  // [a]→ [b]→ [c]→ d]→ [e]→ x
   NiceList.generate = function (start, finish) {
     start = start || 'a';
-    finish = finish || 'j';
+    finish = finish || 'e';
     var this_node = null;
     for (var n = finish.charCodeAt(0); n >= start.charCodeAt(0); n--) {
       this_node = new ListNode(String.fromCharCode(n), this_node);
@@ -26,10 +26,9 @@ var NiceList = (function () {
     return new NiceList(this_node);
   };
 
-  // [a]→ [b]→ [c]→ [d]→ [e]→ [f]→ [g]→ [h]→ [i]→ [j]
-  // !!! WARNING !!!            ↖_________________/
+  // [a]→ [b]→ [c]→ d]→ [e]→ [c]→ ... 
   NiceList.prototype.loop_back = function (value) {
-    value = value || 'f';
+    value = value || 'c';
     var target = null;
     var cursor = this.head;
     while (cursor.next) {
@@ -101,23 +100,89 @@ var NiceList = (function () {
     }
     return new NiceList(head);
   };
+
+  NiceList.prototype.join = function (other, offset) {
+    offset = offset || 0;
+    if (this.has_cycle()) {
+      return false;
+    }
+    var last = this.head;
+    while(last.next) {
+      last = last.next;
+    }
+    for (var s = 0; s < offset; s++) {
+      other.head = other.head.next;
+    }
+    last.next = other.head;
+    return true;
+  };
+
+  NiceList.prototype.palindromal = function () {
+    var slow = this.head;
+    var fast = this.head;
+    if (slow.next === null || slow.next === slow) {
+      return true;
+    }
+    var count = 0;
+    while (fast && fast.next && fast.next.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+      count++;
+      if (slow === fast) {
+        return false; // cycles
+      }
+    }
+    if (fast.next) {
+      count++; // correct for even number
+    }
+    while (slow.next) {
+      count--;
+      fast = this.head;
+      for (var i = 0; i < count; i++) {
+        fast = fast.next;
+      }
+      slow = slow.next;
+      if (fast.value != slow.value) {
+        return false;
+      }
+    }
+    return true;
+  };
   return NiceList;
 })();
 
-var some = NiceList.generate();
-console.log(some.to_string());
-some.reverse();
-console.log(some.to_string());
-var last = some.reversed();
-console.log(last.to_string());
-some.loop_back();
-console.log(some.to_string());
+var deep = NiceList.generate('a', 'j');
+console.log(deep.to_string());
+deep.reverse();
+console.log(deep.to_string());
+var last = NiceList.generate();
+var one = last.reversed();
+var two = last.reversed();
+var bad = last.reversed();
+one.join(last);
+two.join(last, 1);
+bad.join(last, 2);
+console.log(one.to_string());
+console.log('palindromal: ', one.palindromal());
+console.log(two.to_string());
+console.log('palindromal: ', two.palindromal());
+console.log(bad.to_string());
+console.log('palindromal;', bad.palindromal());
+bad.loop_back();
+console.log(bad.to_string());
+console.log('palindromal;', bad.palindromal());
 
 /*
 
 [a]→ [b]→ [c]→ [d]→ [e]→ [f]→ [g]→ [h]→ [i]→ [j]→ x
 [j]→ [i]→ [h]→ [g]→ [f]→ [e]→ [d]→ [c]→ [b]→ [a]→ x
-[a]→ [b]→ [c]→ [d]→ [e]→ [f]→ [g]→ [h]→ [i]→ [j]→ x
-[j]→ [i]→ [h]→ [g]→ [f]→ [e]→ [d]→ [c]→ [b]→ [a]→ [f]→ ...
+[e]→ [d]→ [c]→ [b]→ [a]→ [a]→ [b]→ [c]→ [d]→ [e]→ x
+palindromal:  true
+[e]→ [d]→ [c]→ [b]→ [a]→ [b]→ [c]→ [d]→ [e]→ x
+palindromal:  true
+[e]→ [d]→ [c]→ [b]→ [a]→ [d]→ [e]→ x
+palindromal; false
+[e]→ [d]→ [c]→ [b]→ [a]→ [d]→ [e]→ [c]→ ...
+palindromal; false
 
 */
