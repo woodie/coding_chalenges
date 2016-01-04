06077b1f136f2aeae7ea5dff6b1e79e502f68b50
John Woodell <woodie@netpress.com>
2016-01-03 23:19:04 -0800

    Sort almost sorted array

---
+++ sort_k_sorted.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from heapq import heappush, heappop
+
+def pq_sort(nums, k):
+  out = []
+  x = 0
+  min_heap = []
+  for _ in range(0, k + 1):
+    if len(nums) > x:
+      heappush(min_heap, nums[x])
+      x +=1
+  while len(nums) > x:
+    heappush(min_heap, nums[x])
+    out.append(heappop(min_heap))
+    x +=1
+  while min_heap:
+    out.append(heappop(min_heap))
+  return out
+
+# two away from final position
+input = [3,-1,2,6,4,5,8,7,9]
+
+print ",".join(map(str, pq_sort(input, 1)))
+print ",".join(map(str, sorted(input)))
---
+++ sort_k_sorted.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require 'fc' # priority_queue_cxx
+
+class Array
+  def pq_sort(k)
+    out = []
+    x = 0
+    min_heap = FastContainers::PriorityQueue.new(:min)
+    k.times do
+      min_heap.push(self[x], self[x]) if self.size > x
+      x +=1
+    end
+    while self.size > x
+      min_heap.push(self[x], self[x])
+      out << min_heap.pop
+      x +=1
+    end
+    while !min_heap.empty?
+      out << min_heap.pop
+    end
+    out
+  end
+end
+
+# two away from final position
+input = [3,-1,2,6,4,5,8,7,9]
+
+puts input.pq_sort(1).join(',')
+puts input.sort.join(',')
