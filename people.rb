2e684a8bdcee50ccaa478f44dd21732b049cf6e3
John Woodell <woodie@netpress.com>
2019-05-17 11:14:53 -0700

    Parse a some data from a CSV file.

---
+++ people.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+require 'csv'
+
+NOT_FOUND = "<Not Found>"
+
+class People
+
+  def initialize(file)
+    @ids = {}
+    @names = {}
+    @values = {}
+
+    options = {:headers => true, :header_converters => :symbol}
+    @data = CSV.foreach(file, options).map { |r| r.to_h }
+    @data.each_with_index do |o, i|
+      @ids[o[:id]] = i
+      @names[o[:name]] = i
+      @values[o[:value]] = [] unless @values.has_key?(o[:value])
+      @values[o[:value]] << i
+    end
+  end
+
+  def with_id(id)
+    @ids.has_key?(id.to_s) ?  @data[@ids[id.to_s]] : NOT_FOUND
+  end
+
+  def with_name(name)
+    @names.has_key?(name) ? @data[@names[name]] : NOT_FOUND
+  end
+
+  def with_value(value)
+    return NOT_FOUND unless @values.has_key?(value.to_s)
+    @values[value.to_s].map {|i| @data[i]}
+  end
+
+end
+
+e = People.new('people.csv')
+puts e.with_id(2)
+puts e.with_name('Jane Doe')
+puts e.with_name('John Doe') # NOT_FOUND
+puts e.with_value(40) # John Smith, Biff Henderson
