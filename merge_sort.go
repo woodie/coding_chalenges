a69a9977f4ef17a4411415a3b06c94bdcd951e61
John Woodell <woodie@netpress.com>
2019-10-08 00:57:07 -0700

    Readability over performance.

--- merge_sort.go
+++ merge_sort.go
@@ -12,27 +12,17 @@ func mergeSort(items []int) []int {
 }
 
 func merge(left, right []int) (result []int) {
-  result = make([]int, len(left)+len(right))
-  i := 0
+  result = []int{}
   for len(left) > 0 && len(right) > 0 {
     if left[0] < right[0] {
-      result[i] = left[0]
+      result = append(result,left[0])
       left = left[1:]
     } else {
-      result[i] = right[0]
+      result = append(result,right[0])
       right = right[1:]
     }
-    i++
   }
-  for j := 0; j < len(left); j++ {
-    result[i] = left[j]
-    i++
-  }
-  for j := 0; j < len(right); j++ {
-    result[i] = right[j]
-    i++
-  }
-  return
+  return append(result, append(left, right...)...)
 }
 
 func main() {
