0446435a2a65cd79aab4a613fa3ef0d874449383
John Woodell <woodie@netpress.com>
2022-04-11 15:21:48 -0700

    Replace spaces with tabs.

--- complement.again.go
+++ complement.again.go
@@ -11,20 +11,20 @@ import (
 // and you may not use the same element twice.
 
 func complement(nums []int, target int) []int {
-  println(fmt.Sprintf("# Given nums %d and target %d", nums, target))
-  complements := make(map[int]bool)
+	println(fmt.Sprintf("# Given nums %d and target %d", nums, target))
+	complements := make(map[int]bool)
 
 	for idx := 0; idx < len(nums); idx++ {
 		num := nums[idx]
 		cmp := target - num
-    if (complements[cmp]) { return []int{cmp, num} }
-    complements[num] = true
-  }
-  return []int{}
+		if (complements[cmp]) { return []int{cmp, num} }
+		complements[num] = true
+	}
+	return []int{}
 }
 
 func main() {
-  fmt.Println(complement([]int{2, 7, 11, 15}, 9))
+	fmt.Println(complement([]int{2, 7, 11, 15}, 9))
 }
 
 // Input: nums = [2,7,11,15], tatget = 9
