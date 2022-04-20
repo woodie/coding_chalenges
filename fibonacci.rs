c708dd1eb28f34a78ae160914c24e8d816edb597
John Woodell <woodie@netpress.com>
2022-04-20 01:13:06 -0700

    Add a rust implementation to the mix.
    golang... 0.371331 seconds
    rust..... 0.727000 seconds
    jruby.... 2.416441 seconds
    ruby..... 6.518143 seconds
    python... 11.313397 seconds

---
+++ fibonacci.rs
@@ -0,0 +1,17 @@
+//usr/bin/env rustc $0 -o a.out && ./a.out && rm ./a.out ; exit
+use std::time::Instant;
+
+fn fibonacci(n: u32) -> u32 {
+  match n {
+    0 => 1,
+    1 => 1,
+    _ => fibonacci(n - 1) + fibonacci(n - 2),
+  }
+}
+
+fn main() {
+  let start = Instant::now();
+  fibonacci(40);
+  let elapsed = start.elapsed().as_millis() as f64;
+  println!("rust..... {:.6} seconds", 0.001 * elapsed);
+}
--- fibonacci.sh
+++ fibonacci.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ./fibonacci.go
+./fibonacci.rs
 jruby fibonacci.rb
 ruby fibonacci.rb
 ./fibonacci.py
