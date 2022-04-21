7287e21b165d0f832f32fe25291ae58e89f69b6c
John Woodell <woodie@netpress.com>
2022-04-21 12:27:30 -0700

    Clean up instructions.

--- fleetwood.rb
+++ fleetwood.rb
@@ -1,5 +1,17 @@
 #!/usr/bin/env ruby
 
+# Given ordered lists of band members who enter or exit the studio,
+# write a function that returns two collections:
+#
+# 1. All who didn't use their badge while exiting the room,
+#    they recorded an enter without a matching exit.
+#
+# 2. All who didn't use their badge while entering the room,
+#    they recorded an exit without a matching enter. (The room is empty when the log begins)
+#
+# Each collection should contain no duplicates, regardless of how
+# many times a band member matches the criteria for belonging to it.
+
 badge_records_1 = [
   ["Lindsey", "exit"],
   ["Mic", "enter"],
@@ -22,10 +34,13 @@ badge_records_1 = [
   ["Mic", "exit"],
   ["Mic", "exit"]
 ]
+# Expected output: ["Mic", "John", "Stevie"], ["Lindsey", "John", "Mic"]
+
 badge_records_2 = [
   ["Mic", "enter"],
   ["Mic", "exit"]
 ]
+# Expected output: [], []
 
 badge_records_3 = [
   ["Mic", "enter"],
@@ -33,6 +48,7 @@ badge_records_3 = [
   ["Mic", "exit"],
   ["Mic", "exit"]
 ]
+# Expected output: ["Mic"], ["Mic"]
 
 badge_records_4 = [
   ["Mic", "enter"],
@@ -40,6 +56,7 @@ badge_records_4 = [
   ["Mic", "exit"],
   ["Mic", "enter"]
 ]
+# Expected output: ["Mic"], ["Mic"]
 
 def check_list(records)
   entering = []
@@ -71,71 +88,6 @@ def check_list(records)
 end
 
 puts check_list(badge_records_1).inspect
-puts
 puts check_list(badge_records_2).inspect
-puts
 puts check_list(badge_records_3).inspect
-puts
 puts check_list(badge_records_4).inspect
-
-__END__
-
-The band has a security issue.
-
-Given ordered lists of band members who enter or exit the studio,
-write a function that returns two collections:
-
-1. All who didn't use their badge while exiting the room,
-   they recorded an enter without a matching exit.
-
-2. All who didn't use their badge while entering the room,
-   they recorded an exit without a matching enter. (The room is empty when the log begins)
-
-Each collection should contain no duplicates, regardless of how
-many times a band member matches the criteria for belonging to it.
-
-badge_records_1 = [
-  ["Lindsey",   "exit"],
-  ["Mic",       "enter"],
-  ["Lindsey",   "enter"],
-  ["Stevie",    "enter"],
-  ["Lindsey",   "exit"],
-  ["Christine", "enter"],
-  ["Mic",       "enter"],
-  ["John",      "exit"],
-  ["John",      "enter"],
-  ["Mic",       "exit"],
-  ["Lindsey",   "enter"],
-  ["Lindsey",   "exit"],
-  ["Christine", "exit"],
-  ["Mic",       "enter"],
-  ["Mic",       "enter"],
-  ["Lindsey",   "exit"],
-  ["Mic",       "enter"],
-  ["Mic",       "enter"],
-  ["Mic",       "exit"],
-  ["Mic",       "exit"],
-]
-Expected output: ["Mic", "John", "Stevie"], ["Lindsey", "John", "Mic"]
-
-badge_records_2 = [
-  ["Mic", "enter"],
-  ["Mic", "exit"],
-]
-Expected output: [], []
-
-badge_records_3 = [
-  ["Mic", "enter"],
-  ["Mic", "enter"],
-  ["Mic", "exit"],
-  ["Mic", "exit"],
-]
-Expected output: ["Mic"], ["Mic"]
-
-badge_records_4 = [
-  ["Mic", "enter"],
-  ["Mic", "exit"],
-  ["Mic", "exit"],
-  ["Mic", "enter"],
-]
-Expected output: ["Mic"], ["Mic"]
