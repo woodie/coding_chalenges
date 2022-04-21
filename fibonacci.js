fe2f213b3dbdbf801cc6e3558a09b7983365f907
John Woodell <woodie@netpress.com>
2022-04-21 00:50:20 -0700

    Put node (javascript) in the mix.
    golang... 0.354257 seconds
    rust..... 0.670000 seconds
    node..... 1.016000 seconds
    jruby.... 2.276019 seconds
    ruby..... 6.572543 seconds
    python... 11.345154 seconds

---
+++ fibonacci.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+function fib(n) {
+	if (n < 2) {
+		return n;
+	}
+	return fib(n-1) + fib(n-2);
+}
+
+const start = Date.now();
+fib(40);
+const millis = Date.now() - start;
+console.log(`node..... ${(millis / 1000).toFixed(6)} seconds`);
--- fibonacci.sh
+++ fibonacci.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 ./fibonacci.go
 ./fibonacci.rs
+./fibonacci.js
 jruby fibonacci.rb
 ruby fibonacci.rb
 ./fibonacci.py
