bbc86dffc1eaedca76c13e6bfb89b889d3e25912
John Woodell <woodie@netpress.com>
2022-05-20 22:58:03 -0700

    Add an elixir implementation.

--- fibonacci.bash
+++ fibonacci.bash
@@ -4,6 +4,17 @@
 cc fibonacci.c -o fibonacci.out; ./fibonacci.out
 ./fibonacci.rs
 ./fibonacci.js
+./fibonacci.exs
 jruby fibonacci.rb
 ruby fibonacci.rb
 ./fibonacci.py
+
+# java..... 0.166000 seconds
+# golang... 0.243190 seconds
+# c........ 0.376225 seconds
+# rust..... 0.425000 seconds
+# node..... 0.582000 seconds
+# elixir... 1.384001 seconds
+# jruby.... 1.985102 seconds
+# ruby..... 6.572034 seconds
+# python... 11.42842 seconds
---
+++ fibonacci.exs
@@ -0,0 +1,13 @@
+#!/usr/bin/env elixir
+
+defmodule Main do 
+  def fibonacci(0) do 0 end
+  def fibonacci(1) do 1 end
+  def fibonacci(2) do 2 end
+  def fibonacci(n) do fibonacci(n-1) + fibonacci(n-2) end
+end
+
+start = :os.system_time(:millisecond)
+Main.fibonacci(40)
+secs = (:os.system_time(:millisecond) - start) * 0.001
+IO.puts "elixir... #{Float.ceil(secs, 6)} seconds"
